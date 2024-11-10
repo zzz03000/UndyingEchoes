35fe676bc54d7a47907dbb28e5b9751479d9ad28
zzz03000 <gustp1234567@naver.com>
2024-11-10 18:29:08 +0900

    Gun, Shoot Setting
    총과 슈팅, 기본 세팅

--- Assets/Asset/14 - Zombie/Assets/Scripts/GunData.cs
+++ Assets/Asset/14 - Zombie/Assets/Scripts/GunData.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class GunData
+[CreateAssetMenu(menuName = "Scriptable/GunData", fileName = "Gun Data")]
+public class GunData : ScriptableObject
 {
     public AudioClip shotClip; // 발사 소리
     public AudioClip reloadClip; // 재장전 소리
