cd64f83cc4e5959467f12381e9085583dc8c7ea4
Danny <83389499+danielhjpark@users.noreply.github.com>
2024-11-21 14:29:08 +0900

    FIX::플레이어 마우스 바라보기
    좌우 rotate 주석처리
    PlayerMovement스크립트 내 LookMouseCursor() 함수 추가

--- Assets/Asset/14 - Zombie/Assets/Scripts/PlayerMovement.cs
+++ Assets/Asset/14 - Zombie/Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,16 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 
+    [SerializeField]
+    Camera chatactorCamera;
+
     private void Start() 
     {
         // 사용할 컴포넌트들의 참조를 가져오기
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        //chatactorCamera = GetComponent<Camera>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
@@ -24,7 +28,10 @@ public class PlayerMovement : MonoBehaviour
     {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
         // 회전 실행
-        Rotate();
+        //Rotate();
+
+        //마우스 방향으로 움직임 !!수정!!
+        LookMouseCursor();
         // 움직임 실행
         Move();
 
@@ -43,12 +50,23 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate() 
+    /*private void Rotate() 
     {
         // 상대적으로 회전할 수치 계산
         float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
 
         // 리지드바디를 이용해 게임 오브젝트 회전 변경
         playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }*/
+
+    public void LookMouseCursor()
+    {
+        Ray ray = chatactorCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitResult;
+        if (Physics.Raycast(ray, out hitResult))
+        {
+            Vector3 mouseDir = new Vector3(hitResult.point.x, transform.position.y, hitResult.point.z) - transform.position;
+            playerAnimator.transform.forward = mouseDir;
+        }
     }
 }
