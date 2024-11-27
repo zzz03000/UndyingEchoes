d44959ddde44688a91d380d7fc68c14e4af0de61
Danny <83389499+danielhjpark@users.noreply.github.com>
2024-11-27 18:19:35 +0900

    FIX::플레이어 이동 롤백

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 // 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
 public class PlayerInput : MonoBehaviourPun
 {
-    public string verticalAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
-    public string horizontalAxisName = "Horizontal"; // 좌우 움직임을 위한 입력축 이름
+    public string moveAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
+    public string rotateAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
     public string fireButtonName = "Fire1"; // 발사를 위한 입력 버튼 이름
     public string reloadButtonName = "Reload"; // 재장전을 위한 입력 버튼 이름
 
     // 값 할당은 내부에서만 가능
-    public float moveVertical { get; private set; } // 감지된 움직임 입력값
-    public float moveHorizontal { get; private set; } // 감지된 회전 입력값
+    public float move { get; private set; } // 감지된 움직임 입력값
+    public float rotate { get; private set; } // 감지된 회전 입력값
     public bool fire { get; private set; } // 감지된 발사 입력값
     public bool reload { get; private set; } // 감지된 재장전 입력값
 
@@ -29,18 +29,17 @@ public class PlayerInput : MonoBehaviourPun
         if (GameManager.instance != null
             && GameManager.instance.isGameover)
         {
-            moveVertical = 0;
-            moveHorizontal = 0;
+            move = 0;
+            rotate = 0;
             fire = false;
             reload = false;
             return;
         }
 
         // move에 관한 입력 감지
-        moveVertical = Input.GetAxis(verticalAxisName);
-        moveHorizontal = Input.GetAxis(horizontalAxisName); // A/D
-        /*// rotate에 관한 입력 감지
-        rotate = Input.GetAxis(rotateAxisName);*/
+        move = Input.GetAxis(moveAxisName);
+        // rotate에 관한 입력 감지
+        rotate = Input.GetAxis(rotateAxisName);
         // fire에 관한 입력 감지
         fire = Input.GetButton(fireButtonName);
         // reload에 관한 입력 감지
